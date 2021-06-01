b9af2d15a437578947957d24d86f4ae67408575a
thanhlee74 <thanhhx1998@gmail.com>
2021-06-01 08:25:31 +0700

    add something

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+// import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import ListEmployeeComponent from './components/ListEmployeeComponents';
+// import HeaderComponent from './components/HeaderComponent';
+// import FooterComponent from './components/FooterComponents';
+// import CreateEmployeeComponent from './components/CreateEmployeeComponents';
+// import UpdateEmployeeComponent from './components/UpdateEmployeeComponents';
+// import ViewEmployeeComponent from './components/ViewEmployeeComponents';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <ListEmployeeComponent/>
     </div>
   );
 }
